07e11b9d2bef538f9d1bbae11d91f79904391a46
IT21242472 <99546116+IT21242472@users.noreply.github.com>
2022-02-23 03:35:09 +0000

    Tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,23 @@
 
 int main() {
   
+  int mark1 , mark2 ,sum;
+	float average;
+	 
+	printf("enter the first subject mark:"); 
+	scanf("%d", &mark1);
+	
+	printf("enter the second subject mark:"); 
+	scanf("%d", &mark2);
+
+	sum = (mark1 + mark2);
+
+
+	average = (sum/2);
+
+	printf("average is %.2f", average);
+  
+  
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,26 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+ 	int distance,Amount;
+	
+	printf("Enter the distance:");//user input
+	scanf("%d",&distance);
+	
+	if(distance <= 30)
+	{
+		
+		Amount = 50 * distance;
+		
+		
+		}
+	else if(distance > 30){
+	
+	
+			Amount = 30 * 50 +(distance-30)*40;
+		}
+	
+	printf("Amount is %d",Amount); 
   
   return 0;
 }
